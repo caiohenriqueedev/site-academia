01f8902bc35911c22433c43681388a7663ef1179
Caio Henrique Ramos de Souza <caiohenriquebr12@gmail.com>
2025-04-20 00:29:24 -0300

    adicionando site sobre musculação

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Hello World!");
+
+// Seleciona o botão pelo id
+const alertButton = document.getElementById('alertButton');
+
+// Adiciona um evento de clique ao botão
+alertButton.addEventListener('click', function() {
+  alert('Lembre-se de sempre consultar um profissional antes de iniciar novos treinos!');
+});
